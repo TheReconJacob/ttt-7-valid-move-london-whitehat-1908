6c62e8ccd4b1c83cda7e48988c38ae79c5be420b
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-07 11:19:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(move)
-  position_taken? == false && index > 8
+  index > 8
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
